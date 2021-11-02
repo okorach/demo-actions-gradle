d71a367af2aeeb693db8a0692ee717756617efab
Olivier Korach <olivier.korach@gmail.com>
2021-11-02 14:31:12 +0100

    Some bug

--- comp-gradle/src/main/java/com/acme/App.java
+++ comp-gradle/src/main/java/com/acme/App.java
@@ -2,7 +2,10 @@ package com.acme;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        int i;
+        for (i=0; i<10; i++) {
+            return "Hello world.";
+        }
     }
 
     public static void main(String[] args) {
