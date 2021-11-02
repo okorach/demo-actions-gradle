41ae2ccc44ae3c91475f1e8b03099c800a041187
Olivier Korach <olivier.korach@gmail.com>
2021-11-02 14:39:49 +0100

    Put a better bug

--- comp-gradle/src/main/java/com/acme/App.java
+++ comp-gradle/src/main/java/com/acme/App.java
@@ -3,10 +3,10 @@ package com.acme;
 public class App {
     public String getGreeting() {
         int i;
-        for (i=0; i<10; i++) {
-            return "Hello world.";
+        for (i=0; i>10; i++) {
+            // do nothing
         }
-        return "Hello nothing";
+        return "Hello World.";
     }
 
     public static void main(String[] args) {
