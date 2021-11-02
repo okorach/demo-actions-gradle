7d6ce1247eec21fbe8ec1f38141c96bd3ea3d634
Olivier Korach <olivier.korach@gmail.com>
2021-11-02 14:33:56 +0100

    Fix compilation

--- comp-gradle/src/main/java/com/acme/App.java
+++ comp-gradle/src/main/java/com/acme/App.java
@@ -6,6 +6,7 @@ public class App {
         for (i=0; i<10; i++) {
             return "Hello world.";
         }
+        return "Hello nothing";
     }
 
     public static void main(String[] args) {
