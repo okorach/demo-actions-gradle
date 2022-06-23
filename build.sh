384168bfe233d4eb7e18c04d963db30447343c9c
Olivier Korach <olivier.korach@gmail.com>
2022-06-23 15:57:41 +0200

    Move one level up

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Run each build.sh script in underlying comp-* directories
-for d in *; do
-    if [[ -d $d ]] && [[ $d =~ comp-.* ]]
-    then
-        cd $d; ./build.sh $*; cd -
-    fi
-done
+gradlew jacocoTestReport sonarqube $*
+
+exit $?
